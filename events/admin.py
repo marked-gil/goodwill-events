2f7377472769a89680d004df31bf6a7bb382337c
marked-gil <markgil_creates@yahoo.com>
2022-10-17 00:57:26 +0000

    Install Summernote and apply it in the admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Event
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Event)
+
+@admin.register(Event)
+class EventAdmin(SummernoteModelAdmin):
+    summernote_fields = ('post_content',)
+    prepopulated_fields = {"slug": ("title", "event_date",)}
+    list_display = ('title', 'event_date', 'event_time',
+                    'total_likes', 'status',)
+    search_fields = ['title', 'event_date']
+    list_filter = ('status', 'event_date',)
