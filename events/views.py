ec1e5fc465a03319b7e5eea5d42f4a5aa64b7d0b
marked-gil <markgil_creates@yahoo.com>
2022-10-17 12:39:46 +0000

    Create FeaturedView class

--- events/views.py
+++ events/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Event
 
-# Create your views here.
+
+class FeaturedView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['featured_events'] = Event.objects.all()[:3]
+        return context
