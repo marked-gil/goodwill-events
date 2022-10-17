dd1f9c014b7d5ae77f38846af4917d3606541282
marked-gil <markgil_creates@yahoo.com>
2022-10-17 14:04:49 +0000

    Display the soonest events as featured

--- events/views.py
+++ events/views.py
@@ -8,5 +8,5 @@ class FeaturedView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['featured_events'] = Event.objects.all()[:3]
+        context['featured_events'] = Event.objects.all().reverse()[:3]
         return context
