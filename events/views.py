2bf9a2fe8c1ea8a618b2c3a8cd69dda71f68ce81
marked-gil <markgil_creates@yahoo.com>
2022-10-21 15:58:13 +0000

    Import DetailView

--- events/views.py
+++ events/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from .models import Event
 
 
