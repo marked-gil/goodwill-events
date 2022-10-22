544ad3b40c8b779a4fbd4426c3d12cb985b46070
marked-gil <markgil_creates@yahoo.com>
2022-10-22 16:31:18 +0000

    Create Sign In function-based view

--- members/views.py
+++ members/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def sign_in(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('featured_events')
+
+        else:
+            messages.success(request, ("There was an error logging in."))
+            return redirect('sign_in')
+
+    else:
+        return render(request, './members/login.html', {})
