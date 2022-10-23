6c4c6cd04a56b7807e316cee088131b23fdaa926
marked-gil <markgil_creates@yahoo.com>
2022-10-23 16:55:57 +0000

    Add sign out view

--- members/views.py
+++ members/views.py
@@ -13,8 +13,16 @@ def sign_in(request):
             return redirect('featured_events')
 
         else:
-            messages.success(request, ("There was an error logging in."))
+            messages.success(request, (
+                "Username and password did not match. Try again!"
+                ))
             return redirect('sign_in')
 
     else:
         return render(request, './members/login.html', {})
+
+
+def sign_out(request):
+    logout(request)
+    messages.success(request, ("Your are succesfully signed out."))
+    return redirect('/')
