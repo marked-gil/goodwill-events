211127f75cecbd5640914b0763ad1956ebd67b14
marked-gil <markgil_creates@yahoo.com>
2022-10-24 17:18:26 +0000

    Create URL path for signing up

--- members/urls.py
+++ members/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('sign-in', views.sign_in, name='sign_in'),
     path('sign-out', views.sign_out, name='sign_out'),
+    path('sign_up', views.SignUp.as_view(), name='sign_up'),
 ]
