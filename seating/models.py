fad86f7eacbb20cb0d451c832fbb4a06bc9fa2da
marked-gil <markgil_creates@yahoo.com>
2022-10-25 22:44:06 +0000

    Create EventSeating model

--- seating/models.py
+++ seating/models.py
@@ -14,3 +14,21 @@ class VenueSeat(models.Model):
 
     def __str__(self):
         return self.seat_location
+
+
+class EventSeating(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, related_name='seats')
+    seat_location = models.ForeignKey(
+        VenueSeat, on_delete=models.CASCADE, related_name='event_seating')
+    reserved_on = models.DateTimeField(auto_now_add=True)
+    reserved_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='event_seats')
+
+    class Meta:
+        ordering = ['-reserved_on']
+        unique_together = [['event', 'seat_location']]
+
+    def __str__(self):
+        return f"{self.event.title} on {self.event.event_date}"
