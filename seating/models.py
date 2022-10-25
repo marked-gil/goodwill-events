28fc1f81893e9e59eee15d8e59bfc50e79ad5776
marked-gil <markgil_creates@yahoo.com>
2022-10-25 22:41:38 +0000

    Create VenueSeat Model

--- seating/models.py
+++ seating/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from events.models import Event
+import uuid
 
-# Create your models here.
+
+class VenueSeat(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    seat_location = models.CharField(max_length=10, unique=True, blank=False)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['seat_location']
+
+    def __str__(self):
+        return self.seat_location
