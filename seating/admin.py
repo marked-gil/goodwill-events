dc2d7bb5370777466642e9cdd5cd575a710eb030
marked-gil <markgil_creates@yahoo.com>
2022-10-25 22:45:06 +0000

    Registr VenueSeat and EventSeating models to admin

--- seating/admin.py
+++ seating/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import VenueSeat, EventSeating
 
-# Register your models here.
+
+@admin.register(VenueSeat)
+class VenueSeatAdmin(admin.ModelAdmin):
+    list_display = ('seat_location', 'is_active',)
+
+
+@admin.register(EventSeating)
+class EventSeatingAdmin(admin.ModelAdmin):
+    list_display = ('event', 'seat_location', 'reserved_by', 'reserved_on',)
+    search_fields = ('event', 'seat_location',)
+    list_filter = ('event', 'seat_location',)
