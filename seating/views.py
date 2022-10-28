a87faa9a6bb68749f3364623624b54fb9f1bb5f5
marked-gil <markgil_creates@yahoo.com>
2022-10-28 00:08:37 +0000

    Create EventSeatsView

--- seating/views.py
+++ seating/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from .models import EventSeating
 
-# Create your views here.
+
+class EventSeatsView(View):
+    def get(self, request, slug, *args, **kwargs):
+        event_seats_obj = EventSeating.objects.filter(event__slug=slug)
+        list_seats = []
+        num_iteration = 1
+        for item in event_seats_obj:
+            list_seats.append(str(item.seat_location))
+            if num_iteration == 1:
+                event_name = item.event
+        return render(request, './seating/reserve-seats.html', {'data': list_seats, 'event': event_name})
