57bcb34b199d496bd06df7384d4dc2b5d089332c
marked-gil <markgil_creates@yahoo.com>
2022-10-28 14:13:42 +0000

    Show on screen selected seats, and remove deselected seats

--- seating/static/seating/js/seating_script.js
+++ seating/static/seating/js/seating_script.js
@@ -2,14 +2,33 @@ window.onpageshow = function () {
     const reservedSeatsList = document.getElementById('data_seats').textContent;
     const seatsList = document.querySelectorAll('[data-seat-location]');
     for (let seat of seatsList) {
-        var seat_loc = seat.getAttribute("data-seat-location")
+        const seat_loc = seat.getAttribute("data-seat-location")
         if (reservedSeatsList.includes(seat_loc)) {
             seat.style.fill = "blue";
         } else {
             seat.style.cursor = 'pointer'
-            seat.addEventListener('click', (e) => {
-                seat.classList.toggle("booked")
+            seat.addEventListener('click', function () {
+                this.classList.toggle("booked");
+                showSelectedSeat(this, seat_loc);
+                removeDeselectedSeat(this, seat_loc);
             })
         }
+    }
+}
+
+function showSelectedSeat(seat, seat_loc) {
+    if (seat.classList.contains("booked")) {
+        const li_elem = document.createElement("li");
+        li_elem.innerText = seat_loc;
+        document.getElementById("seats_selected_list").appendChild(li_elem)
+        li_elem.setAttribute('id', seat_loc)
+    }
+}
+
+
+function removeDeselectedSeat(seat, seat_loc) {
+    if (!seat.classList.contains("booked")) {
+        const li_elem = document.getElementById(seat_loc);
+        li_elem.remove()
     }
 }
