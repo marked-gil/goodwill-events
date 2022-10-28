d9908244fb539d7f3641cb4aa607b46a2ccb8ba3
marked-gil <markgil_creates@yahoo.com>
2022-10-28 10:04:11 +0000

    Handle raised error when no object in EventSeating model exists

--- seating/views.py
+++ seating/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.views import View
+from events.models import Event
 from .models import EventSeating
 
 
 class EventSeatsView(View):
     def get(self, request, slug, *args, **kwargs):
-        event_seats_obj = EventSeating.objects.filter(event__slug=slug)
-        list_seats = []
-        for item in event_seats_obj:
-            list_seats.append(str(item.seat_location))
-            if num_iteration == 1:
-                event_name = item.event
-        return render(request, './seating/reserve-seats.html', {
-            'data': list_seats,
-            'event': event_name,
-            })
+        try:
+            event = Event.objects.filter(slug=slug).first()
+            event_seats_obj = EventSeating.objects.filter(event__slug=slug)
+            if not event_seats_obj.exists():
+                raise Exception("Start making seat reservations for this event.")
+        except Exception as info:
+            return render(request, './seating/reserve-seats.html', {
+                'event': event
+                })
+        else:
+            list_seats = []
+            for item in event_seats_obj:
+                list_seats.append(str(item.seat_location))
+            return render(request, './seating/reserve-seats.html', {
+                'data': list_seats,
+                'event': event,
+                })
