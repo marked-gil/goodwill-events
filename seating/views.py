52e5db6327288b4b4f9de110b00c7d1290955f9f
marked-gil <markgil_creates@yahoo.com>
2022-10-28 00:58:56 +0000

    Add skeletal structure for Colour legend and selected seats

--- seating/views.py
+++ seating/views.py
@@ -7,10 +7,11 @@ class EventSeatsView(View):
     def get(self, request, slug, *args, **kwargs):
         event_seats_obj = EventSeating.objects.filter(event__slug=slug)
         list_seats = []
-        num_iteration = 1
-        event_name = ""
         for item in event_seats_obj:
             list_seats.append(str(item.seat_location))
             if num_iteration == 1:
                 event_name = item.event
-        return render(request, './seating/reserve-seats.html', {'data': list_seats, 'event': event_name})
+        return render(request, './seating/reserve-seats.html', {
+            'data': list_seats,
+            'event': event_name,
+            })
