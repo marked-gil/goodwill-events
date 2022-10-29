a475196cf45815ffe85a0f585ae59fe66fca92e2
marked-gil <markgil_creates@yahoo.com>
2022-10-29 00:33:26 +0000

    Allow only 2 seats to be reserved per user

--- seating/static/seating/js/seating_script.js
+++ seating/static/seating/js/seating_script.js
@@ -1,34 +1,46 @@
 window.onpageshow = function () {
     const reservedSeatsList = document.getElementById('data_seats').textContent;
     const seatsList = document.querySelectorAll('[data-seat-location]');
+    ALLOWED_SEATS_PER_USER = 2
+    num_seats_chosen = 0
     for (let seat of seatsList) {
-        const seat_loc = seat.getAttribute("data-seat-location")
+        const seat_loc = seat.getAttribute("data-seat-location");
         if (reservedSeatsList.includes(seat_loc)) {
             seat.style.fill = "blue";
         } else {
             seat.style.cursor = 'pointer'
             seat.addEventListener('click', function () {
-                this.classList.toggle("booked");
-                showSelectedSeat(this, seat_loc);
-                removeDeselectedSeat(this, seat_loc);
+                if (num_seats_chosen < ALLOWED_SEATS_PER_USER) {
+                    num_seats_chosen += toggleSeat(this, seat_loc);
+                } else {
+                    if (seat.classList.contains("booked")) {
+                        num_seats_chosen += toggleSeat(seat, seat_loc)
+                    }
+                }
             })
         }
     }
 }
 
-function showSelectedSeat(seat, seat_loc) {
+function toggleSeat(seat, seat_loc) {
+    seat.classList.toggle("booked");
     if (seat.classList.contains("booked")) {
-        const li_elem = document.createElement("li");
-        li_elem.innerText = seat_loc;
-        document.getElementById("seats_selected_list").appendChild(li_elem)
-        li_elem.setAttribute('id', seat_loc)
+        showSelectedSeat(seat_loc);
+        return 1
+    } else {
+        removeDeselectedSeat(seat_loc);
+        return -1
     }
 }
 
+function showSelectedSeat(seat_loc) {
+    const li_elem = document.createElement("li");
+    li_elem.innerText = seat_loc;
+    document.getElementById("seats_selected_list").appendChild(li_elem)
+    li_elem.setAttribute('id', seat_loc)
+}
 
-function removeDeselectedSeat(seat, seat_loc) {
-    if (!seat.classList.contains("booked")) {
-        const li_elem = document.getElementById(seat_loc);
-        li_elem.remove()
-    }
+function removeDeselectedSeat(seat_loc) {
+    const li_elem = document.getElementById(seat_loc);
+    li_elem.remove()
 }
