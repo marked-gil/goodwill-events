abc429952df09a293564e040d0ba3b801093df5c
marked-gil <markgil_creates@yahoo.com>
2022-10-31 02:17:31 +0000

    Modify EventSeating admin display

--- seating/admin.py
+++ seating/admin.py
@@ -4,11 +4,12 @@ from .models import VenueSeat, EventSeating
 
 @admin.register(VenueSeat)
 class VenueSeatAdmin(admin.ModelAdmin):
-    list_display = ('seat_location', 'is_active',)
+    list_display = ('seat_location', 'is_active')
 
 
 @admin.register(EventSeating)
 class EventSeatingAdmin(admin.ModelAdmin):
-    list_display = ('event', 'seat_location', 'reserved_by', 'reserved_on',)
-    search_fields = ('event', 'seat_location',)
-    list_filter = ('event', 'seat_location',)
+    list_display = ('event', 'seat_location_1', 'seat_location_2',
+                    'reserved_by', 'reserved_on')
+    search_fields = ('event', 'reserved_by')
+    list_filter = ('event', 'reserved_by')
