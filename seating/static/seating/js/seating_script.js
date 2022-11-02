cf161ee50fa0c17fbe65d380b99486a017002ab0
marked-gil <markgil_creates@yahoo.com>
2022-11-02 02:55:39 +0000

    Clear specific seat on seatmap when it is cancelled

--- seating/static/seating/js/seating_script.js
+++ seating/static/seating/js/seating_script.js
@@ -1,8 +1,8 @@
 if (document.getElementById('seatmap-container')) {
+    const reservedSeatsList = document.getElementById('data_seats').textContent;
+    const seatsList = document.querySelectorAll('[data-seat-location]');
 
     window.onpageshow = function () {
-        const reservedSeatsList = document.getElementById('data_seats').textContent;
-        const seatsList = document.querySelectorAll('[data-seat-location]');
         ALLOWED_SEATS_PER_USER = 2
         num_seats_chosen = 0
         for (let seat of seatsList) {
@@ -25,9 +25,30 @@ if (document.getElementById('seatmap-container')) {
     }
     
     // EventListener for form submission
-    const reserve_button = document.getElementById("submit_reservation");
-    reserve_button.addEventListener('click', submitSeatReservation)
-    
+    if (document.getElementById("submit_reservation")) {
+        const reserve_button = document.getElementById("submit_reservation");
+        reserve_button.addEventListener('click', submitSeatReservation)
+    }
+
+    // EventListener for 'remove' reserved seat button
+    if (document.getElementsByClassName("btn_seat_location")) {
+        const remove_seat_buttons = document.querySelectorAll('.btn_seat_location')
+        remove_seat_buttons.forEach(function (btn) {
+            btn.addEventListener('click', function () {
+                seat = this.parentElement
+                removeDeselectedSeat(seat);
+
+                for (seat_box of seatsList) {
+                    seat_location = seat_box.getAttribute("data-seat-location")
+                    if (seat_location == seat.getAttribute("id")) {
+                        seat_box.style.fill = ""
+                    }
+                }
+
+            })
+        })
+    }
+
     
     // FUNCTIONS <---
     
@@ -50,15 +71,19 @@ if (document.getElementById('seatmap-container')) {
     }
     
     function removeDeselectedSeat(seat_loc) {
-        const li_elem = document.getElementById(seat_loc);
-        li_elem.remove()
+        if (typeof seat_loc == "string") {
+            const li_elem = document.getElementById(seat_loc)
+            li_elem.remove()
+        } else if (typeof seat_loc == "object") {
+            seat_loc.remove()
+        }
     }
-    
-    function submitSeatReservation() {
+
+    function fillSeatReservationForm() {
         const selected_seats_list = document.querySelectorAll("#seats_selected_list>li");
         const select_seat_1 = document.querySelector("#id_seat_location_1");
         const select_seat_2 = document.querySelector("#id_seat_location_2");
-    
+
         for (let i = 0; i < selected_seats_list.length; i++) {
             const seat_text = selected_seats_list[i].innerText
             let seat_field = i == 0 ? select_seat_1 : select_seat_2
@@ -66,7 +91,10 @@ if (document.getElementById('seatmap-container')) {
             const option_selected = options_list.filter(item => item.text == seat_text)
             seat_field.value = option_selected[0].value
         }
-    
+    }
+
+    function submitSeatReservation() {
+        fillSeatReservationForm()
         // submit form
         const seat_form = document.getElementById("reserve_seat_form")
         seat_form.submit();
