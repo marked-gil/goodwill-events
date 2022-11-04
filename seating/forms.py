6ae1e2e39dcd5daefa823fb6f7b62ac427ca589f
marked-gil <markgil_creates@yahoo.com>
2022-11-04 00:31:26 +0000

    Add 'user_limit_reached' to the fields passed into the reserve-seats page

--- seating/forms.py
+++ seating/forms.py
@@ -5,4 +5,4 @@ from .models import EventSeating
 class SeatReserveForm(ModelForm):
     class Meta:
         model = EventSeating
-        fields = ['seat_location_1', 'seat_location_2']
+        fields = ['seat_location_1', 'seat_location_2', 'user_limit_reached']
