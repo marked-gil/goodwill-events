e13bd5cd5b328337f7335315d750bf06af8568ac
marked-gil <markgil_creates@yahoo.com>
2022-11-04 19:02:19 +0000

    Add DEBUG_PROPAGATE_EXCEPTIONS as True to help in debugging on heroku

--- goodwill_events/settings.py
+++ goodwill_events/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
