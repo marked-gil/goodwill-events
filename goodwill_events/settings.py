d3e1a10b9e4626e360ae63777a9456c2704e881c
marked-gil <markgil_creates@yahoo.com>
2022-11-04 12:19:26 +0000

    Set Debug to False if on production mode

--- goodwill_events/settings.py
+++ goodwill_events/settings.py
@@ -16,7 +16,7 @@ import os
 if os.path.isfile('env.py'):
     import env
 
-development = os.environ.get('DEVELOPMENT', True)
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
