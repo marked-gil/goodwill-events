f81f3a1ae73599b69a3ed4c38833921237fad8d2
marked-gil <markgil_creates@yahoo.com>
2022-11-11 15:32:18 +0000

    Create Comment characters counter

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+if (document.getElementById('comment-form')) {
+    // Comment Characters Counter
+    MAX_COMMENT_CHARS = 250
+    const comment_textarea = document.getElementById('comment_textarea')
+    const char_counter_field = document.getElementById('char-counter')
+
+    comment_textarea.addEventListener("keyup", (e) => {
+        const comment_length = comment_textarea.value.length
+        const chars_remaining = MAX_COMMENT_CHARS - comment_length
+
+        if (comment_length <= MAX_COMMENT_CHARS) {
+            let chars = chars_remaining > 1 ? "characters" : "character"
+            char_counter_field.textContent = `${chars_remaining} ${chars} remaining`
+        }
+    })
+}
