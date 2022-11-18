a58bc7f6547075e9ed9c12c27d48804e4dba5d7b
marked-gil <markgil_creates@yahoo.com>
2022-11-18 21:52:04 +0000

    Remove unused codes

--- members/urls.py
+++ members/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('sign-in', views.sign_in, name='sign_in'),
-    path('sign-out', views.sign_out, name='sign_out'),
-    path('sign_up', views.SignUp.as_view(), name='sign_up'),
+
 ]
