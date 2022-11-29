6c7e8e09ecd876f67f176ce42be094821f287821
marked-gil <markgil_creates@yahoo.com>
2022-11-29 19:45:44 +0000

    Refactor DeleteSeatsReservation to minimize verbosity, and add comments to classes and functions

--- seating/views.py
+++ seating/views.py
@@ -11,16 +11,30 @@ from django.conf import settings
 
 
 class EventSeatsView(LoginRequiredMixin, View):
+    """
+    Displays the seat map page and all the seats reserved for a particular
+    event as requested by a logged-in user.
+    Also redirects to the UpdateSeatsReservation view if the logged-in user
+    has already booked a seat/s for the event.
+    """
     permission_denied_message = 'You need to sign in to make seat reservations.'
 
-    # Displays a message to tell user to sign in before making reservation
     def dispatch(self, request, *args, **kwargs):
+        """
+        Displays a message to tell user to sign in before making reservation
+        """
         if not request.user.is_authenticated:
             messages.error(request, self.permission_denied_message)
             return self.handle_no_permission()
         return super(EventSeatsView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, slug, *args, **kwargs):
+        """
+        Retrieves the requested event and its reserved seats, and returns the
+        Seat Reservation template with details of the specific event.
+        Redirects to the UpdateReservationView if user has already booked
+        seat/s for the event.
+        """
         reservation_form = SeatReserveForm()
         try:
             event = Event.objects.filter(slug=slug).first()
@@ -50,6 +64,10 @@ class EventSeatsView(LoginRequiredMixin, View):
                 })
 
     def post(self, request, slug, *args, **kwargs):
+        """
+        Saves to database the seat/s booked by the user in response to a POST
+        request method.
+        """
         event = Event.objects.filter(slug=slug).first()
         user = request.user
         reservation_form = SeatReserveForm(data=request.POST)
@@ -65,8 +83,15 @@ class EventSeatsView(LoginRequiredMixin, View):
 
 
 class UpdateSeatsReservation(LoginRequiredMixin, View):
-
+    """
+    Displays the seat/s reserved by the logged-in user, and updates the
+    database if the user makes changes to their seat reservation.
+    """
     def get(self, request, slug, *args, **kwargs):
+        """
+        Retrieves the requested event and user's booked seats and returns the
+        Seat Reservation template for updating the booked seats.
+        """
         reservation_form = SeatReserveForm()
 
         event = Event.objects.filter(slug=slug).first()
@@ -85,6 +110,12 @@ class UpdateSeatsReservation(LoginRequiredMixin, View):
             'user_booked_seats': user_booked_seats})
 
     def post(self, request, slug, *args, **kwargs):
+        """
+        Updates the user's seat reservation/s for a particular event in the
+        database in response to POST request.
+        Redirects to DeleteSeatsReservation view if reservation form is
+        emptied.
+        """
         user = request.user
         event_seats_obj = EventSeating.objects.filter(
             event__slug=slug)
@@ -99,7 +130,9 @@ class UpdateSeatsReservation(LoginRequiredMixin, View):
                 updated_form = reservation_form.save(commit=False)
                 updated_form.save()
             else:
-                raise Exception
+                raise Exception(
+                    "Reservation form is empty. This may mean you want to delete your reserved seats."
+                )
         except Exception:
             formdata_dict = dict(request.POST)
             empty_seat_1 = formdata_dict['seat_location_1'] == ['']
@@ -116,21 +149,29 @@ class UpdateSeatsReservation(LoginRequiredMixin, View):
 
 
 class DeleteSeatsReservation(LoginRequiredMixin, DeleteView):
+    """
+    Deletes the seats reserved by a logged-in user for a particular event
+    """
+
     model = EventSeating
 
     def get_object(self, queryset=None):
+        """
+        Retrieves and returns the seats booked for a particular event
+        """
         slug = self.kwargs.get('slug')
         event_seats_obj = EventSeating.objects.filter(
             event__slug=slug)
+
         return event_seats_obj
 
-    def delete(self, request, slug, *args, **kwargs):
-        event_seats_obj = EventSeating.objects.filter(
-            event__slug=slug)
-        self.object = event_seats_obj.filter(
-            reserved_by=request.user).first()
+    def delete(self, request, *args, **kwargs):
+        """
+        Deletes the seats reserved by the user for an event.
+        """
+        self.object = self.get_object().filter(
+            reserved_by=request.user
+            ).first()
+        success_url = f'/{self.kwargs.get("slug")}/reserve-seat/'
         self.object.delete()
-        messages.success(request,
-                         'Your reservation for this event is successfully DELETED.'
-                         )
-        return redirect(f'/{self.kwargs.get("slug")}/reserve-seat/')
+        return redirect(success_url)
