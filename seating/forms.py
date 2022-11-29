13a31c3722dda90d30f56d0f2935a54a1093caa8
marked-gil <markgil_creates@yahoo.com>
2022-11-29 19:55:11 +0000

    Add comment to class SeatReserveForm

--- seating/forms.py
+++ seating/forms.py
@@ -3,6 +3,9 @@ from .models import EventSeating
 
 
 class SeatReserveForm(ModelForm):
+    """
+    Seat Reservation form class based on the Event Seating Model
+    """
     class Meta:
         model = EventSeating
         fields = ['seat_location_1', 'seat_location_2', 'user_limit_reached']
