840e8225c3e3c431338dfa2de32ef1451547c415
marked-gil <markgil_creates@yahoo.com>
2022-11-30 19:31:01 +0000

    Add comment to CommentForm class

--- events/forms.py
+++ events/forms.py
@@ -3,6 +3,9 @@ from .models import Comment
 
 
 class CommentForm(ModelForm):
+    """
+    A form class for Comment model with a field for text_comment.
+    """
     class Meta:
         model = Comment
         fields = ['text_comment']
