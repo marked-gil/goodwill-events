1f9ac799137df7d2b711cfde99157c2b0251e63c
marked-gil <markgil_creates@yahoo.com>
2022-12-26 12:35:34 +0000

    Check if DEVELOPMENT is in os environment for DEBUG

--- goodwill_events/settings.py
+++ goodwill_events/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['goodwill-events.herokuapp.com', 'localhost']
