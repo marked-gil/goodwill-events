3639a13ae1b337685b108b419c121b92e0e29c5b
marked-gil <markgil_creates@yahoo.com>
2023-01-04 12:15:50 +0000

    Set DEBUG to True for debugging in Heroku

--- goodwill_events/settings.py
+++ goodwill_events/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['goodwill-events.herokuapp.com', 'localhost']
