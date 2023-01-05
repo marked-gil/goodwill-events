abd871cd3d64319e0ba43b20c8d0cf8fac9dfe64
marked-gil <markgil_creates@yahoo.com>
2023-01-05 06:47:22 +0000

    Prepopulate author and entered_by fields in Event Admin with current user

--- events/admin.py
+++ events/admin.py
@@ -6,11 +6,22 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
     summernote_fields = ('post_content',)
-    prepopulated_fields = {"slug": ("title", "event_date",)}
+    prepopulated_fields = {"slug": ("title",)}
     list_display = ('title', 'event_date', 'event_time',
                     'total_likes', 'status',)
     search_fields = ['title', 'event_date']
     list_filter = ('status', 'event_date',)
+
+    def get_form(self, request, *args, **kwargs):
+        """
+        Add the current user as the default value for the 'author'
+        and 'entered_by' fields
+        """
+        form = super(EventAdmin, self).get_form(
+            request, *args, **kwargs)
+        form.base_fields['author'].initial = request.user
+        form.base_fields['entered_by'].initial = request.user
+        return form
 
 
 @admin.register(Comment)
