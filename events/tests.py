a345fe84c1650430c64080aaa002bf5ea9654c6c
marked-gil <markgil_creates@yahoo.com>
2023-01-20 16:12:10 +0000

    Add automated tests for rendering pages

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from .models import Event
+from datetime import datetime, date, time
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.test import Client
 
-# Create your tests here.
+
+class TestEventsViews(TestCase):
+    """
+    Tests Views that belong to 'events' app
+    """
+
+    def test_home_page(self):
+        """
+        Tests the correct rendering of the Home page
+        """
+        url = reverse('featured_events')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/index.html')
+        self.assertContains(response, 'Featured Events')
+
+    def test_events_page(self):
+        """
+        Tests the correct rendering of the Events page
+        """
+        url = reverse('events_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/events.html')
+        self.assertContains(response, 'Upcoming Events')
+
+    def test_view_seatmap(self):
+        """
+        Tests the correct rendering of the generic Seat Map page
+        """
+        url = reverse('view_seatmap')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/view_seatmap.html')
+        self.assertContains(response, 'Seat Map')
--- goodwill_events/settings.py
+++ goodwill_events/settings.py
@@ -116,10 +116,16 @@ WSGI_APPLICATION = 'goodwill_events.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
