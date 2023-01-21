10dc7d3585c4cf288da2c5eb8fb39ce702ee8014
marked-gil <markgil_creates@yahoo.com>
2023-01-21 17:58:07 +0000

    Tests for incorrect login passwords, too common passwords, and invalid email

--- member/tests.py
+++ member/tests.py
@@ -17,17 +17,8 @@ class BaseTestCase(TestCase):
         self.username = 'voltes_v'
         self.first_name = 'Mark'
         self.last_name = 'Dacutan'
-        self.password = 'japanes3_anime145$'
         self.email = 'mark@mail.com'
-
-        self.user_register = {
-            'username': self.username,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'password1': self.password,
-            'password2': self.password,
-        }
+        self.password = 'japanes3_anime145$'
 
 
 class TestRegistration(BaseTestCase):
@@ -44,10 +35,47 @@ class TestRegistration(BaseTestCase):
     def test_register_user(self):
         """ Tests user signup on the Register page """
         response = self.client.post(
-            self.register_url, self.user_register
+            self.register_url, {
+                'username': self.username,
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'email': self.email,
+                'password1': self.password,
+                'password2': self.password,
+                }
             )
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('featured_events'))
+
+    def test_too_common_password(self):
+        """ Tests signup response to very common passwords """
+        response = self.client.post(
+            self.register_url, {
+                'username': self.username,
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'email': self.email,
+                'password1': 12345678,
+                'password2': 12345678,
+                }
+            )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'This password is too common.')
+
+    def test_invalid_email(self):
+        """ Tests signup response to invalid email """
+        response = self.client.post(
+            self.register_url, {
+                'username': self.username,
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'email': 'anime@',
+                'password1': self.password,
+                'password2': self.password,
+                }
+            )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Enter a valid email address.')
 
 
 class TestLogin(BaseTestCase):
@@ -77,6 +105,14 @@ class TestLogin(BaseTestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('featured_events'))
         self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_incorrect_login_password(self):
+        response = self.client.post(self.login_url, {
+            'login': self.username, 'password': 'wrongpasswoRD154'
+            })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'The username and/or password you ' +
+                            'specified are not correct.')
 
     def test_user_logout(self):
         """ Tests user logout """
