b3ea9f85c77d0fce8166df0d4d48684f819572fb
marked-gil <markgil_creates@yahoo.com>
2023-01-21 14:19:49 +0000

    Refactor the TestEventsViews and add test for rendering of specific event page

--- events/tests.py
+++ events/tests.py
@@ -10,13 +10,17 @@ class TestEventsViews(TestCase):
     """
     Tests Views that belong to 'events' app
     """
+    def setUp(self):
+        self.home_url = reverse('featured_events')
+        self.events_url = reverse('events_list')
+        self.generic_seatmap_url = reverse('view_seatmap')
+        self.specific_event_url_name = 'event_details'
 
     def test_home_page(self):
         """
         Tests the correct rendering of the Home page
         """
-        url = reverse('featured_events')
-        response = self.client.get(url)
+        response = self.client.get(self.home_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'events/index.html')
         self.assertContains(response, 'Featured Events')
@@ -25,8 +29,7 @@ class TestEventsViews(TestCase):
         """
         Tests the correct rendering of the Events page
         """
-        url = reverse('events_list')
-        response = self.client.get(url)
+        response = self.client.get(self.events_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'events/events.html')
         self.assertContains(response, 'Upcoming Events')
@@ -35,8 +38,16 @@ class TestEventsViews(TestCase):
         """
         Tests the correct rendering of the generic Seat Map page
         """
-        url = reverse('view_seatmap')
-        response = self.client.get(url)
+        response = self.client.get(self.generic_seatmap_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'events/view_seatmap.html')
         self.assertContains(response, 'Seat Map')
+
+    def test_specific_event_page(self):
+        """
+        Tests the correct rendering of the Event Details page
+        """
+        response = self.client.get(reverse(
+            self.specific_event_url_name, kwargs={'slug': 'test-event'}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('events/event_details.html')
