5c5c886b2f427483bf00c25dec8f0cbf7b8e2c9a
marked-gil <markgil_creates@yahoo.com>
2023-01-25 16:52:39 +0000

    Remove unused imports

--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect
 from django.http import JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
--- seating/views.py
+++ seating/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, View, DeleteView
 from django.contrib import messages
